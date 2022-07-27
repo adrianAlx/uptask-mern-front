40eebaf44a5b7f0627f38a2834a1489f47af7978
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-07-27 12:08:03 -0500

    [ADD]: header component added

---
+++ src/components/Heder.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <header className="px-4 py-5 bg-white border-b">
+      <div className="md:flex md:justify-between">
+        <h2 className="text-4xl text-sky-600 font-black text-center mb-5 md:mb-0">
+          <Link to="/projects"> UpTask</Link>
+        </h2>
+
+        <div className="flex flex-col md:flex-row items-center gap-4">
+          <button type="button" className="font-bold uppercase">
+            Buscar Proyecto
+          </button>
+
+          <Link to="/projects" className="font-bold uppercase">
+            Proyectos
+          </Link>
+
+          <button
+            type="button"
+            className="text-white text-sm bg-sky-600 p-3 rounded-md uppercase font-bold"
+          >
+            Cerrar Sesión
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
