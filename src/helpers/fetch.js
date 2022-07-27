4d8b6fce9ea0803637f0e8b453a338a5095d8e9e
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-07-27 12:04:19 -0500

    [ADD]: fetchWithToken added

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -15,3 +15,16 @@ export const fetchWithoutToken = async (endpoint, data, method = 'GET') => {
   if (method === 'GET') return await axiosClient(endpoint);
   else return await axiosClient(endpoint, setConfig(method, data));
 };
+
+export const fetchWithToken = async (endpoint, method = 'GET', token, data) => {
+  if (method === 'GET')
+    return await axiosClient(endpoint, {
+      method,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  else {
+    return await axiosClient(endpoint, setConfig(method, data, token));
+  }
+};
