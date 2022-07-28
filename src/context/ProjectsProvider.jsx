e65f9cd372ac0a3f1851f2be7a67f15251c2abf8
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-07-28 09:16:26 -0500

    [ADD]: Add new project added

--- src/context/ProjectsProvider.jsx
+++ src/context/ProjectsProvider.jsx
@@ -1,12 +1,61 @@
-import { createContext, useState } from 'react';
+import { createContext, useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getJwtFromLS } from '../helpers/checkJWTInLS';
+import { fetchWithToken } from '../helpers/fetch';
 
 export const ProjectContext = createContext();
 
 export const ProjectsProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [projects, setProjects] = useState([]);
+  const [project, setProject] = useState({});
+  const [alerta, setAlerta] = useState({});
+
+  const setAlert = useCallback(
+    alert => {
+      setAlerta(alert);
+
+      setTimeout(() => {
+        setAlerta({});
+      }, 3000);
+    },
+    [setAlerta]
+  );
+
+  const submitProject = async project => {
+    console.log(project);
+    project.id ? 'await updateProject(project)' : await newProject(project);
+  };
+
+  const newProject = async project => {
+    const tokenJWT = getJwtFromLS();
+    if (!tokenJWT) return;
+
+    try {
+      const { data } = await fetchWithToken(
+        '/projects',
+        'POST',
+        tokenJWT,
+        project
+      );
+      setProjects([...projects, data.project]);
+
+      setAlerta({ msg: data.msg, error: false });
+
+      setTimeout(() => {
+        setAlert({});
+        navigate('/projects', { replace: true });
+      }, 2100);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <ProjectContext.Provider value={{ projects }}>
+    <ProjectContext.Provider
+      value={{ projects, alerta, project, setAlert, submitProject }}
+    >
       {children}
     </ProjectContext.Provider>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,9 @@
+import { useProjects } from '../hooks';
+
 export const Projects = () => {
+  const { projects } = useProjects();
+  console.log(projects);
+
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
