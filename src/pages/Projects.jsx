e33a53b80b701ee468fa9e62dd7adfcb561ce1a8
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-07-28 09:25:17 -0500

    [ADD]: Projects page added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { useProjects } from '../hooks';
+import { ProjectPreview } from '../components';
 
 export const Projects = () => {
   const { projects } = useProjects();
@@ -8,7 +9,17 @@ export const Projects = () => {
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
 
-      <div className="bg-white shadow mt-10 rounded-lg">Projects</div>
+      <div className="bg-white shadow mt-10 rounded-lg">
+        {projects.length ? (
+          projects.map(project => (
+            <ProjectPreview key={project._id} project={project} />
+          ))
+        ) : (
+          <p className="text-center text-gray-600 uppercase p-5">
+            Aún no tienes proyectos
+          </p>
+        )}
+      </div>
     </>
   );
 };
